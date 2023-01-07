45521aaf8380b9c652e2380947a53bdb343855a3
siddharthsingla <37585197+siddharthsingla@users.noreply.github.com>
2023-01-07 15:30:49 -0600

    guessing_game

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,7 @@
 import random
 def guessing_game():
     random_integer = random.randint(0, 100)
-    while guess := int(input("Please enter a number between 0 and 100")):
+    while guess := int(input("Please enter a number between 0 and 100: ")):
         if guess == random_integer:
             print(f"You guessed it right {random_integer}")
             break
@@ -9,3 +9,5 @@ def guessing_game():
             print(f"{guess} is low")
         else:
             print(f"{guess} is high")
+
+guessing_game()
